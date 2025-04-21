62e4423eb0c753f17a347029e4ab1a27b35bc77e
Younghwan <158720833+Ryan0hwan@users.noreply.github.com>
2025-04-22 00:04:43 +0900

    fix: 코드수정

--- Udemy/6. DP/2748(1).py
+++ Udemy/6. DP/2748(1).py
@@ -1,16 +1,18 @@
 # 백준 2748. 피보나치수 2
 # https://www.acmicpc.net/problem/2748
 
-# 1. top-down 방식(재귀. memoization 방식)
-"""def f(n):
+""" 기본으로 알고있는 코드(시간초과)
+def f(n):
     if n == 0: return 0
     elif n == 1: return 1
     else:
         return f(n-2) + f(n-1)
 
-print(f(int(input())))"""
+print(f(int(input())))
+"""
 
-## 아직 memoization을 적용하지 않은 상태. => 따라서 시간초과 발생.
+# 1. top-down 방식(재귀. memoization 방식)
+## 위 코드는 아직 memoization을 적용하지 않은 상태. => 따라서 시간초과 발생.
 ## 코드 수정.
 
 cache = [-1]*91     # memoization을 위해, dp테이블을 만들어둠.  음수값을 저장(구분위해)
