34ebfdbbc799d6e2b6c784c91f7d5d8e009b42f1
Younghwan <158720833+Ryan0hwan@users.noreply.github.com>
2025-05-03 21:39:58 +0900

    fix: 코드수정

--- StartPS/Strings/8-14.py
+++ StartPS/Strings/8-14.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 
+# 텍스트파일 가져와서 읽기
 infile = open('GasPrices.txt', 'r')
 lines = infile.readlines()
 infile.close()
-
 
 # '/n' 제거
 for i in range(len(lines)):
     lines[i] = lines[i].rstrip('\n')
 
+# 하나씩 필요한 것만 남기기
 years = []
 prices = []
 for line in lines:
@@ -20,10 +21,10 @@ for line in lines:
     prices.append(price)
 
 # 그래프의 x, y 설정
-x = [year for year in range(years[0], years[-1]+1)]  # x: 1993년 ~ 2013년; 
+x = [year for year in range(years[0], years[-1]+1)]  # x: 1993년 ~ 2013년
 y = []                                               # y: 연도별 값의 평균
 for year in x:
-   year_prices = []
+   year_prices = []                                  # 연도별로 평균구하기
    for i in range(len(years)):
       if year == years[i]:
          year_prices.append(prices[i])
@@ -37,7 +38,7 @@ print(y)
 # 그래프 그리기 시작
 plt.plot(x, y, 'o--')
 
-# x(년도)가 2년마다 표시되게끔
+# x(년도)가 2년마다 표시되게끔 xticks 이용
 interval = []
 for i in range(0, len(x), 2):
    interval.append(x[i])
