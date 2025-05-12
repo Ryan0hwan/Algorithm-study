caa1cfa9b880b67f9fad4c8c6cbf0c39f4ddff46
Younghwan <158720833+Ryan0hwan@users.noreply.github.com>
2025-05-12 22:32:43 +0900

    Create 모의담금질.py

---
+++ SWEA/모의담금질.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+T = int(input())
+
+for i in range(T):
+   T, T_end, k = int(input().split())
+
+def cost():
+   
+
+
+
+for _ in range(T):
+   print(f'#{i} {cost()}')
