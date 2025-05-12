13e0aee6ebadcd5c3913feef5de7c6e193534017
Younghwan <158720833+Ryan0hwan@users.noreply.github.com>
2025-05-12 22:46:33 +0900

    fix: 코드수정

--- SWEA/모의담금질.py
+++ SWEA/모의담금질.py
@@ -3,13 +3,18 @@ import math
 
 T = int(input())
 
-for i in range(T):
-   T, T_end, k = int(input().split())
-
 def cost():
-   
+   return random.random()
 
+for i in range(1,4):
+   T, T_end, k = map(float, (input().split()))
+   count = 0; cost_pre = float('inf')
 
+   while T > T_end:
+      cost_new = cost(); count += 1
+      diff = cost_new - cost_pre
+      if diff < 0 or math.exp(-diff/T) > random.random():
+         cost_pre = cost_new
+      T = T*k
 
-for _ in range(T):
-   print(f'#{i} {cost()}')
+   print(f'#{i} {count}')
